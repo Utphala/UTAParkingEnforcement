f272b3db8a786a0588bcdc6e1211a737f944ee86
Utphala <utphala.p@gmail.com>
2016-03-30 20:48:16 -0500

    Adding screens for buy, add, cancel, renew permit and payment

---
+++ app/src/main/java/edu/uta/mavs/utaparkingenforcement/cancel_permit.java
@@ -0,0 +1,13 @@
+package edu.uta.mavs.utaparkingenforcement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cancel_permit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancel_permit);
+    }
+}
---
+++ app/src/main/java/edu/uta/mavs/utaparkingenforcement/dummy_list_of_tow_service.java
@@ -0,0 +1,13 @@
+package edu.uta.mavs.utaparkingenforcement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dummy_list_of_tow_service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_list_of_tow_service);
+    }
+}
